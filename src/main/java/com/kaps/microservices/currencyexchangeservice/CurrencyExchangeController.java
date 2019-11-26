40cb397bc9bc82590e887f0a04b94ef144f2c37a
Kapeel Mopkar <kapeel_mopkar@persistent.com>
2019-11-26 13:52:11 +0530

    JPA changes for Currency Exchange added

---
+++ src/main/java/com/kaps/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.kaps.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private ExchangeValueRepository exchangeValueRepository;
+	
+	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		/*
+		 * ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to,
+		 * BigDecimal.valueOf(65));
+		 * exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")))
+		 * ;
+		 */
+		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return exchangeValue;
+	}
+
+}
